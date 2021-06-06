6923218af65128a813e5bcf46f871526bab0c15b
Juan Chavez <jmchv2001@gmail.com>
2021-06-05 23:21:47 -0700

    read-me updated

--- README.md
+++ README.md
@@ -1,3 +1,50 @@
-# Professional README Generator Starter Code
+# README Generator
 
-[How to create a Professional README](./readme-guide.md)
+<div style="position: absolute; top: 22px; right: 50px">
+
+![licence: MIT](https://img.shields.io/badge/license-MIT-blue)
+</div>
+
+<a href = "#description"></a>
+## Description
+
+README Generator is a command line app that helps you quickly make a high quality README.md, so you can spend more time developing and less time documenting.
+
+Clone this repo to use readme-generator on your machine.
+
+## Table of Contents
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+  - [Questions](#questions)
+
+<a href = "#installation"></a>
+## Installation
+- Clone the repo to a directory on your machine.
+- Make sure you have node installed on your machine.
+- Make sure to run npm init to install the required dependencies listed in package-lock.json.
+
+<a href = "#usage"></a>
+## Usage
+- From the root directory "readme-generator" run the command "node app". 
+- You will then be shown a series of prompts. Make sure to answer all of them!
+- Once you have finished answering the prompts your high quality README.md file will be located in the dist directory 
+
+<a href = "#credits"></a>
+## Credits
+- [jchavezval](https://github.com/jchavezval)
+
+
+<a href = "#license"></a>
+## License
+- MIT
+
+<a href = "questions"></a>
+## Questions
+Feel free to reach out with any question you have about README Generator!
+
+### Contact information:
+- GitHub: [jchavezval](https://github.com/jchavezval)
+- Email: [jmchv2001@gmail.com](mailto:jmchv2001@gmail.com)
--- app.js
+++ app.js
@@ -1,164 +1,216 @@
-// packages needed for this application
-
 const inquirer = require('inquirer');
-const generateReadMe = require('./src/ReadMe-template.js');
-const writeReadMe = require('./utils/generateReadMe.js');
+const generateReadme = require('./src/readme-template');
+const writeReadme = require('./utils/generate-readme.js');
 const licenses = require('./utils/licenses.js');
 
-const licenseName = (licenses) => {
-    list = licenses.map((obj = obj => obj.name));
-    return list;
+const licenseNames = (licenses) => {
+  list = licenses.map((obj = obj => obj.name));
+  return list
 }
 
-// An array of questions for user input
-
 const promptUser = () => {
-    return inquirer.prompt([
-        { 
-            type: 'input',
-            name: 'projectName',
-            message: 'What is the name of your project? (Required)',
-            validate: projectInput => {
-                if (projectInput) {
-                    return true;
-                } else {
-                    console.log('You need to enter your project name');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'gitHub',
-            message: 'Enter your GitHub Username (Required)',
-            validate: gitHubInput => {
-                if (gitHubInput) {
-                    return true;
-                } else {
-                    console.log('Please enter your GitHub username!');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: 'Enter your Email address  (Required)',
-            validate: emailInput => {
-                if (emailInput) {
-                    return true;
-                } else {
-                    console.log('Please enter your Email address!');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'description',
-            message: 'Provide a description of your  project (Required)',
-            validate: descriptionInput => {
-                if (descriptionInput) {
-                    return true;
-                } else {
-                    console.log('You need to enter a project description!');
-                    return false;
-                }
-            }
-        },     
-        {
-            type: 'list',
-            name: 'license',
-            message: "Please choose a license",
-            choices: licenseName(licenses)
-        },
-        {
-            type: 'input',
-            name: 'deployedLink',
-            message: 'Enter the deployed GitHub link of your project. (Required)',
-            validate: linkInput => {
-                if (linkInput) {
-                return true;
-                } else {
-                    console.log('You need to enter the url of your application. (Required)');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'installationInstructions',
-            message: 'Enter the instructions of how to install your application. (Required)',
-            validate: linkInput => {
-                if (linkInput) {
-                return true;
-                } else {
-                    console.log('Please enter the instructions to install your application. (Required)');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'usageInstructions',
-            message: 'Enter usage instructions of the application. (Required)',
-            validate: nameInput => {
-                if (nameInput) {
-                return true;
-                } else {
-                    console.log('Please enter usage instructions. (Required)');
-                    return false;
-                }
-            }
+  return inquirer.prompt(
+    [
+      {
+        // project name
+        type: 'input',
+        name: 'projectName',
+        message: 'What is the title of your project? (Required)',
+        validate: nameInput => {
+          if(nameInput) {
+            return true;
+          } else {
+            console.log(`Please enter your project's name.`);
+            return false;
+          }
         }
+      },
+      {
+        // gitHub
+        type: 'input',
+        name: 'gitHub',
+        message: 'What is your GitHub username? (Required)',
+        validate: nameInput => {
+          if(nameInput) {
+            return true;
+          } else {
+            console.log(`Please enter your GitHub username.`);
+            return false;
+          }
+        }
+      },
+      {
+        // email
+        type: 'input',
+        name: 'email',
+        message: 'What is your email? (Required)',
+        validate: nameInput => {
+          if(nameInput) {
+            return true;
+          } else {
+            console.log(`Please enter your email.`);
+            return false;
+          }
+        }
+      },
+      {
+        // description
+        type: 'input',
+        name: 'description',
+        message: 'Please write a short description of your project. What? Why? How?',
+        validate: nameInput => {
+          if(nameInput) {
+            return true;
+          } else {
+            console.log(`Please enter your project's name.`);
+            return false;
+          }
+        }
+      },
+      {
+        // confirm deployed
+        type: 'confirm',
+        name: 'confirmDeployed',
+        message: 'Is your application deployed?',
+        default: false
+      },
+      {
+        // deployed app link
+        type: 'input',
+        name: 'deployedAppLink',
+        message: 'Provide the url of your application? (Required)',
+        when: ({ confirmDeployed }) => {
+          if(confirmDeployed) {
+            return true;
+          } else {
+            return false;
+          }
+        },
+        validate: nameInput => {
+          if(nameInput) {
+            return true;
+          } else {
+            console.log(`Please enter your project's name.`);
+            return false;
+          }
+        }
+      },
+      {
+        // installation
+        type: 'input',
+        name: 'installation',
+        message: 'Enter installation instructions. (Required)',
+        validate: nameInput => {
+          if(nameInput) {
+            return true;
+          } else {
+            console.log(`Please enter installation instructions for your application.`);
+            return false;
+          }
+        }
+      },
+      {
+        // usage
+        type: 'input',
+        name: 'usage',
+        message: 'Enter usage instructions. (Required)',
+        validate: nameInput => {
+          if(nameInput) {
+            return true;
+          } else {
+            console.log(`Please enter usage instructions for your application.`);
+            return false;
+          }
+        }
+      },
+      {
+        // list
+        type: 'list',
+        name: 'license',
+        message: "Please choose a license",
+        choices: licenseNames(licenses)
+      },
+      {
+        // guide lines
+        type: 'input',
+        name: 'guideLines',
+        message: 'Enter Contributing Guide Lines for your application. (Required)',
+        validate: nameInput => {
+          if(nameInput) {
+            return true;
+          } else {
+            console.log(`Please enter Contributing Guide Lines for your application.`);
+            return false;
+          }
+        }
+      },
+      {
+        // test
+        type: 'input',
+        name: 'test',
+        message: 'Enter test instructions for your application. (Required)',
+        validate: nameInput => {
+          if(nameInput) {
+            return true;
+          } else {
+            console.log(`Please enter test instructions for your application.`);
+            return false;
+          }
+        }
+      }
     ]
-    )};
+  )};
 
-    const promptContributors = contributors => {
-        if (!contributors.credits) {
-            contributors.credits=[];
+  const promptCredits = contributors => {
+    if(!contributors.credits) {
+      contributors.credits =[];
+    }
+    return inquirer.prompt([
+      {
+        // confirm
+        type: 'confirm',
+        name: 'confirmAddContributor',
+        message: 'Would you like to add a contributor to your project?',
+        default: false
+      },
+      {
+        // credits
+        type: 'input',
+        name: 'contributorGitHub',
+        message: 'Please enter a contributors GitHub username',
+        when: ({ confirmAddContributor }) => {
+          if(confirmAddContributor) {
+            return true;
+          } else {
+            return false;
+          }
         }
-        return inquirer.prompt([
-            {
-                type: 'confirm',
-                name: 'confirmContributors',
-                message: 'Is there any contributors for your project?',
-                default: false
-            },
-            {
-                type: 'input',
-                name: 'contributorsGitHub',
-                message: 'Please enter the GitHub username of the contributor',
-                when: ({confirmContributors}) => {
-                    if (confirmContributors) {
-                        return true;
-                    } else {
-                        return false;
-                    }
-                }
-            }
+      }
     ])
-    .then(contributorsData => {
-        contributors.credits.push(contributorsData);
-        if (contributorsData.confirmContributors) {
-            return promptContributors(contributors)
-        } else {
-            return contributors
-        }
+    .then(contributingData => {
+      contributors.credits.push(contributingData);
+      if(contributingData.confirmAddContributor) {
+        return promptCredits(contributors)
+      } else {
+        return contributors
+      }
     });
-};
+  };
 
-promptUser()
-    .then(promptContributors)
-    .then(readmeInfo => {
-        console.log(readmeInfo);
-        return generateReadMe(readmeInfo);
-    })
+  promptUser()
+    .then(promptCredits)
     .then(readmeData => {
-        return writeReadMe(readmeData);
+      console.log(readmeData);
+      return generateReadme(readmeData);
+    })
+    .then(readmeContent => {
+      return writeReadme(readmeContent);
     })
     .then(writeResponse => {
-        console.log (`
-        ${writeResponse.message}`);
+      console.log(`
+        ========================================================
+        ${writeResponse.message}
+        ========================================================
+      `);
     })
     .catch(err => console.log(err));
